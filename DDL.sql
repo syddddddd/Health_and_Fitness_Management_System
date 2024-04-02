67612ce9613e048e43cec5faa8e077bed990f65f
syddddddd <93814980+syddddddd@users.noreply.github.com>
2024-04-02 04:48:01 -0400

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -7,12 +7,19 @@ CREATE TABLE Member(
     phone_number VARCHAR(15) NOT NULL,
     birth_date DATE,
     join_date DATE DEFAULT CURRENT_DATE,
+    gender VARCHAR(255) NOT NULL,
+
+    -- login info
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) UNIQUE NOT NULL,
+
+    -- health metrics
     resting_hr INT,
     avg_sleep INT,
     curr_weight INT,
     height INT,
+
+    -- fitness goals
     goal_weight INT,
     distance INT,
     goal_time TIME
@@ -27,9 +34,11 @@ CREATE TABLE Trainer(
     phone_number VARCHAR(15) NOT NULL,
     birth_date DATE,
     join_date DATE DEFAULT CURRENT_DATE,
+    gender VARCHAR(255) NOT NULL,
+
+    -- login info
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) UNIQUE NOT NULL,
-    gender VARCHAR(255)
+    password VARCHAR(255) UNIQUE NOT NULL
 );
 
 --create admins table
@@ -41,6 +50,8 @@ CREATE TABLE Admin(
     phone_number VARCHAR(15) NOT NULL,
     birth_date DATE,
     join_date DATE DEFAULT CURRENT_DATE,
+
+    -- login info
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) UNIQUE NOT NULL
 );
@@ -48,14 +59,17 @@ CREATE TABLE Admin(
 --create schedule table
 CREATE TABLE Schedule(
     time_slot PRIMARY KEY TIME NOT NULL,
-    member_id INT NOT NULL UNIQUE,
-    trainer_id INT NOT NULL UNIQUE,
+    member_id INT REFERENCES Member(member_id),
+    trainer_id INT REFERENCES Trainer(trainer_id),
+
+    -- not particularly neccessary since we already have trainer_id
     trainer_fname VARCHAR(255) NOT NULL,
     trainer_lname VARCHAR(255) NOT NULL,
+
     availability VARCHAR(255) NOT NULL,
     session_type VARCHAR(255) NOT NULL,
-    FOREIGN KEY (member_id)
-        REFERENCES Member,
-    FOREIGN KEY (trainer_id)
-        REFERENCES Trainers
+
 );
+
+-- personal info table for all of them???
+-- keep track of things like names, emails, phones, birth_date, sex

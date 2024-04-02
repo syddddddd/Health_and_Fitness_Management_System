11e5c3372eb25e7e1c767e4b2e8eb67ff856e848
syddddddd <93814980+syddddddd@users.noreply.github.com>
2024-04-01 22:46:50 -0400

    Members Table updated

---
+++ DDL.sql
@@ -0,0 +1,19 @@
+-- create members table
+CREATE TABLE Members(
+    member_id SERIAL PRIMARY KEY,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    birth_date DATE,
+    join_date DATE DEFAULT CURRENT_DATE,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL,
+    resting_hr INT,
+    avg_sleep INT,
+    curr_weight INT,
+    height INT,
+    goal_weight INT,
+    distance INT,
+    time TIME
+);

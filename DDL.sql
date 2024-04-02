27e2966c1deae7099d622763b4101fdba37d3fe1
syddddddd <93814980+syddddddd@users.noreply.github.com>
2024-04-02 04:20:58 -0400

    More tables created

--- DDL.sql
+++ DDL.sql
@@ -17,3 +17,45 @@ CREATE TABLE Members(
     distance INT,
     goal_time TIME
 );
+
+-- create trainers table
+CREATE TABLE Trainer(
+    trainer_id SERIAL PRIMARY KEY,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    birth_date DATE,
+    join_date DATE DEFAULT CURRENT_DATE,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL,
+    gender VARCHAR(255)
+);
+
+--create admins table
+CREATE TABLE Admin(
+    admin_id SERIAL PRIMARY KEY,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    birth_date DATE,
+    join_date DATE DEFAULT CURRENT_DATE,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) UNIQUE NOT NULL
+);
+
+--create schedule table
+CREATE TABLE Schedule(
+    time_slot PRIMARY KEY TIME NOT NULL,
+    member_id INT NOT NULL UNIQUE,
+    trainer_id INT NOT NULL UNIQUE,
+    trainer_fname VARCHAR(255) NOT NULL,
+    trainer_lname VARCHAR(255) NOT NULL,
+    availability VARCHAR(255) NOT NULL,
+    session_type VARCHAR(255) NOT NULL,
+    FOREIGN KEY (member_id)
+        REFERENCES Members,
+    FOREIGN KEY (trainer_id)
+        REFERENCES Trainers
+);
